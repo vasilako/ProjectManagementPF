4f4255da7cba01db2ef56c2f705debb9e5abd1de
vasilako <vasilako@hotmail.com>
2025-06-20 12:50:20 +0200

    feat: support multiple images per product with carousel display
    - Removed image field from Product model
    - Created new ProductImage model with foreign key to Product
    - Registered ProductImage as inline in ProductAdmin
    - Refactored product card template to use Bootstrap carousel with multiple images
    - Fallback to placeholder if no image is available

--- products/admin.py
+++ products/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
+
 
 # Register your models here.
-from django.contrib import admin
-from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "description")
     search_fields = ("name",)
 
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "price")
+    inlines = [ProductImageInline]
     search_fields = ("name", "description",)
     list_filter = ("category",)
+
+
 
--- products/models.py
+++ products/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
 # Create your models here.
-from django.db import models
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -19,12 +18,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(
-        upload_to='products/%Y/%m/%d',
-        blank=True,
-        null=True,
-        default='defaults/no-image-available.png'
-    )
 
     class Meta:
         verbose_name_plural = "Products"
@@ -32,3 +25,11 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+class ProductImage(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/gallery/%Y/%m/%d')
+    caption = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.product.name} - Image"
