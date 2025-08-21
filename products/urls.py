66fda5ebfb1e9f26955554cb8976c7f3f4cfd920
vasilako <vasilako@hotmail.com>
2025-08-21 03:45:51 +0200

    feat(products): add quantity form + link to cart; namespace-safe URLs

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,11 @@
+# products/urls.py
+
 from django.urls import path
 from .views import ProductListView, ProductDetailView
+
+
+app_name = "products"
+
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
