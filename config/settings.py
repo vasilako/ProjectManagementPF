2d991c7ee01ce2cdd9957307b6717585ee45dfce
vasilako <vasilako@hotmail.com>
2025-08-30 15:51:02 +0200

    Fix settings and fix Ensure dir on VPS

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ if USE_POSTGRES:
             'NAME': config('POSTGRES_DB'),
             'USER': config('POSTGRES_USER'),
             'PASSWORD': config('POSTGRES_PASSWORD'),
-            'HOST': config('POSTGRES_HOST', default='localhost'),
+            'HOST': config('POSTGRES_HOST', default='db'),
             'PORT': config('POSTGRES_PORT', default='5432'),
         }
     }
